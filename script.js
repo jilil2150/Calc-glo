a183d779f5df7ad411ce7a96cd7d4181ec2a44b5
Vladislaw Markov <jilil2150@gmail.com>
2025-04-01 00:42:36 +0300

    Второй урок

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-console.log("Привет");
-alert("Hello word!");
-let money, income, addExpenses, deposit, mission, period;
+let money, income, addExpenses, deposit, mission, period, budgetDay;
+
+money=350;
+income='stock exchange';
+addExpenses='auto, internet, restaurants';
+deposite=true;
+mission=1000;
+period=6;
+budgetDay=33000
+
+console.log (typeof money);
+console.log (typeof income);
+console.log (typeof deposite);
+
+console.log (addExpenses.length);
+
+console.log('Период равен', period, 'месяцам');
+
+console.log('Цель заработать', mission, 'рублей');
+
+console.log(addExpenses.toLowerCase(addExpenses));
+
+console.log(addExpenses.split(', '));
+
+console.log(budgetDay/30);
